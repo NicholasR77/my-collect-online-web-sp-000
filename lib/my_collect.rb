9079ba77a016aa9095efc8e5a45252ea9c1702f7
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-23 02:27:35 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,17 +2,16 @@ test_arr = ["test1", "test2"]
 
 def my_collect(array)
   counter = 0
+  new_array = []
 
-  while counter < array.length
-
-    new_array = []
-    if block_given?
+  if block_given?
+    while counter < array.length
       new_array.push(yield(array[counter])
       counter += 1
-    else
-        puts "Please provide a block."
     end
-    arrays
+    new_array
+  else
+      puts "Please provide a block."
   end
 end
 
